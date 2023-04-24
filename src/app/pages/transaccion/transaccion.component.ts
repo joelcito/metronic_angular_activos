fcad475c909a12d2ea3aa66c4a66c506eab46e37
Joel Flores <jjjoelcito123@gmail.com>
2023-04-24 15:56:34 -0400

    se agrego chee

--- src/app/pages/transaccion/transaccion.component.ts
+++ src/app/pages/transaccion/transaccion.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-transaccion',
   templateUrl: './transaccion.component.html',
-  styleUrls: ['./transaccion.component.scss']
 })
 export class TransaccionComponent implements OnInit {
 
